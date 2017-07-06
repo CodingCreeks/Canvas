a12a55aa172e5e4f5c0a44e1423dfa0e819f343b
CodingKnowledge <manjunatha.gunderi@gmail.com>
2017-07-06 17:34:36 -0400

    moutain & star code added

--- js/canvas.js
+++ js/canvas.js
@@ -1,116 +1,263 @@
-  var canvas = document.querySelector("canvas");
-  var c = canvas.getContext('2d');
+		var canvas = document.querySelector('canvas');
+		var c = canvas.getContext('2d');
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
 
-  var mouse = {
-      x: canvas.width / 2,
-      y: canvas.height / 2
-  }
-
-  window.addEventListener("resize", function() {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-  });
-
-  window.addEventListener("mousemove", function(e) {
-      mouse.x = e.clientX;
-      mouse.y = e.clientY;
-  });
-
-  var colors = [
-      { r: 255, g: 153, b: 51 },
-      { r: 19, g: 136, b: 8 },
-      { r: 255, g: 255, b: 255 }
-  ];
-
-  function Particle(x, y, dx, dy, r, ttl) {
-      this.x = x;
-      this.y = y;
-      this.dx = dx;
-      this.dy = dy;
-      this.r = r;
-      this.opacity = 1;
-      this.shouldRemove = false;
-      this.timeToLive = ttl;
-      this.randomColor = Math.floor(Math.random() * colors.length);
+		window.addEventListener("resize", function() {
+		    canvas.width = window.innerWidth;
+		    canvas.height = window.innerHeight;
+		});
 
 
-      this.update = function() {
-          this.x += this.dx;
-          this.y += this.dy;
-          if (this.x + this.r >= canvas.width || this.x - this.r <= 0) {
-              this.dx = -this.dx;
-          };
+		/*
+		 * ------------------------------------------
+		 * *-----------------------------
+		 *  Design
+		 * *-----------------------------
+		 * ------------------------------------------
+		 */
 
-          if (this.y + this.r >= canvas.height || this.y - this.r <= 0) {
-              this.dy = -this.dy;
-          };
+		function Star() {
+		    this.radius = (Math.random() * 10) + 5;
+		    this.x = this.radius + (canvas.width - this.radius * 2) * Math.random();
+		    this.y = -10;
+		    this.dx = (Math.random() - 0.5) * 20;
+		    this.dy = 30;
+		    this.gravity = .5;
+		    this.friction = .54;
 
-          // Ensure that particles cannot be spawned past canvas boundaries
-          this.x = Math.min(Math.max(this.x, 0 + this.r), canvas.width - this.r);
-          this.y = Math.min(Math.max(this.y, 0 + this.r), canvas.height - this.r);
+		    this.update = function() {
 
-          c.beginPath();
-          c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-          // c.strokeStyle = 'rgba(0, 0, 0,' + this.opacity + ')';
-          c.strokeStyle = 'rgba(' + colors[this.randomColor].r + ',' + colors[this.randomColor].g + ',' + colors[this.randomColor].b + ',' + this.opacity + ')';
-          c.fillStyle = 'rgba(' + colors[this.randomColor].r + ',' + colors[this.randomColor].g + ',' + colors[this.randomColor].b + ',' + this.opacity + ')';
-          // c.fill();
-          c.stroke();
-          c.closePath();
+		        // Bounce particles off the floor of the canvas
+		        if (this.y + this.radius + this.dy >= canvas.height - groundHeight) {
+		            this.dy = -this.dy * this.friction;
+		            this.dx *= this.friction;
+		            this.radius -= 3;
 
-          this.opacity -= 1 / (ttl / 0.1);
-          this.r -= r / (ttl / 0.1);
-          this.timeToLive -= 0.1;
-      }
+		            explosions.push(new Explosion(this));
+		        } else {
+		            this.dy += this.gravity;
+		        }
 
-      this.remove = function() {
-          // If timeToLive expires, return a true value.
-          // This signifies that the particle should be removed from the scene.
-          return this.timeToLive <= 0;
-      }
-  }
+		        // Bounce particles off left and right sides of canvas
+		        if (this.x + this.radius + this.dx >= canvas.width || this.x - this.radius + this.dx < 0) {
+		            this.dx = -this.dx;
+		            this.dx *= this.friction;
+		            explosions.push(new Explosion(this));
+		        };
 
-  function Explosion(x, y) {
-      this.particles = [];
+		        // Move particles by velocity
+		        this.x += this.dx;
+		        this.y += this.dy;
 
-      this.init = function() {
-          for (var i = 1; i <= 1; i++) {
-              var randomVelocity = {
-                  x: (Math.random() - 0.5) * 3.5,
-                  y: (Math.random() - 0.5) * 3.5
-              }
-              this.particles.push(new Particle(x, y, randomVelocity.x, randomVelocity.y, 30, 8));
-          }
-      }
+		        this.draw();
 
-      this.init();
+		        // Draw particles from explosion
+		        for (var i = 0; i < explosions.length; i++) {
+		            explosions[i].update();
+		        }
 
-      this.draw = function() {
-          for (var i = 0; i < this.particles.length; i++) {
-              this.particles[i].update();
+		    }
+		    this.draw = function() {
+		        c.save();
+		        c.beginPath();
+		        c.arc(this.x, this.y, Math.abs(this.radius), 0, Math.PI * 2, false);
 
-              if (this.particles[i].remove() == true) {
-                  this.particles.splice(i, 1);
-              };
-          }
-      }
-  }
+		        c.shadowColor = ' #FF9933';
+		        c.shadowBlur = 20;
+		        c.shadowOffsetX = 0;
+		        c.shadowOffsetY = 0;
 
-  var explosions = [];
+		        c.fillStyle = " #FF9933";
+		        c.fill();
+		        c.closePath();
+		        c.restore();
+		    }
+		}
 
-  function animate() {
-      window.requestAnimationFrame(animate);
+		function Particle(x, y, dx, dy) {
+		    this.x = x;
+		    this.y = y;
+		    this.size = {
+		        width: 2,
+		        height: 2
+		    };
+		    this.dx = dx;
+		    this.dy = dy;
+		    this.gravity = .09;
+		    this.friction = 0.88;
+		    this.timeToLive = 3;
+		    this.opacity = 1;
 
-      c.fillStyle = "#1e1e1e";
-      c.fillRect(0, 0, canvas.width, canvas.height);
+		    this.update = function() {
+		        if (this.y + this.size.height + this.dy >= canvas.height - groundHeight) {
+		            this.dy = -this.dy * this.friction;
+		            this.dx *= this.friction;
+		        } else {
+		            this.dy += this.gravity;
+		        }
 
-      explosions.push(new Explosion(mouse.x, mouse.y));
+		        if (this.x + this.size.width + this.dx >= canvas.width || this.x + this.dx < 0) {
+		            this.dx = -this.dx;
+		            this.dx *= this.friction;
+		        };
+		        this.x += this.dx;
+		        this.y += this.dy;
 
-      for (var i = 0; i < explosions.length; i++) {
-          explosions[i].draw();
-      }
-  }
-  animate();
+		        this.draw();
+
+		        this.timeToLive -= 0.01;
+		        this.opacity -= 1 / (this.timeToLive / 0.01);
+		    }
+		    this.draw = function() {
+		        c.save();
+		        c.fillStyle = "rgba(227, 234, 239," + this.opacity + ")";
+		        c.shadowColor = ' #FF9933';
+		        c.shadowBlur = 20;
+		        c.shadowOffsetX = 0;
+		        c.shadowOffsetY = 0;
+		        c.fillRect(this.x, this.y, this.size.width, this.size.height);
+		        c.restore();
+		    }
+
+		    this.isAlive = function() {
+		        return 0 <= this.timeToLive;
+		    }
+		}
+
+		function Explosion(star) {
+		    this.particles = [];
+
+		    this.init = function(parentStar) {
+		        for (var i = 0; i < 8; i++) {
+		            var velocity = {
+		                x: (Math.random() - 0.5) * 5,
+		                y: (Math.random() - 0.5) * 15,
+		            }
+		            this.particles.push(new Particle(parentStar.x, parentStar.y, velocity.x, velocity.y));
+		        }
+		    }
+
+		    this.init(star);
+
+		    this.update = function() {
+		        for (var i = 0; i < this.particles.length; i++) {
+		            this.particles[i].update();
+		            if (this.particles[i].isAlive() == false) {
+		                this.particles.splice(i, 1);
+		            }
+		        }
+		    }
+		}
+
+
+		function createMountainRange(height, yPosition, mountainAmount, color) {
+		    for (var i = 0; i < mountainAmount; i++) {
+		        var width = canvas.width / mountainAmount;
+
+		        // Draw triangle
+		        c.beginPath();
+		        c.moveTo(i * width, yPosition);
+		        c.lineTo(i * width + width + 325, yPosition);
+
+		        // Triangle peak
+		        c.lineTo(i * width + width / 2, yPosition - height / 2);
+		        c.lineTo(i * width - 325, yPosition);
+		        c.fillStyle = color;
+		        c.fill();
+		        c.closePath();
+		    }
+		}
+
+		function MiniStar() {
+		    this.x = Math.random() * canvas.width;
+		    this.y = Math.random() * canvas.height;
+		    this.radius = Math.random() * 3;
+
+		    this.draw = function() {
+		        c.save();
+		        c.beginPath();
+		        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+
+		        c.shadowColor = '#E3EAEF';
+		        c.shadowBlur = (Math.random() * 10) + 10;
+		        c.shadowOffsetX = 0;
+		        c.shadowOffsetY = 0;
+
+		        c.fillStyle = "white";
+		        c.fill();
+
+		        c.closePath();
+		        c.restore();
+		    }
+		}
+
+
+		/*
+		 * ------------------------------------------
+		 * *-----------------------------
+		 *  Implementation
+		 * *-----------------------------
+		 * ------------------------------------------
+		 */
+
+		var timer = 0;
+		var stars = [];
+		var explosions = [];
+		var groundHeight = canvas.height * 0.15;
+		var randomSpawnRate = Math.floor((Math.random() * 25) + 60)
+		var backgroundGradient = c.createLinearGradient(0, 0, 0, canvas.height);
+		backgroundGradient.addColorStop(0, "rgba(23, 30, 38, 0.7)");
+		backgroundGradient.addColorStop(1, "rgba(63, 88, 107, 0.7)");
+
+		var miniStars = [];
+		for (var i = 0; i < 150; i++) {
+		    miniStars.push(new MiniStar());
+		}
+
+
+
+
+		function animate() {
+		    window.requestAnimationFrame(animate);
+		    c.fillStyle = backgroundGradient;
+		    c.fillRect(0, 0, canvas.width, canvas.height);
+
+		    for (var i = 0; i < miniStars.length; i++) {
+		        miniStars[i].draw();
+		    }
+		    createMountainRange(canvas.height / .55, canvas.height, 1, "#384551");
+		    createMountainRange(canvas.height / .7, canvas.height, 2, "#2B3843");
+		    createMountainRange(canvas.height / 1.2, canvas.height, 3, "#26333E");
+
+		    c.fillStyle = "#182028";
+		    c.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight);
+
+
+
+		    for (var i = 0; i < stars.length; i++) {
+		        stars[i].update();
+		        // console.log(stars[0].isAlive());
+
+		        if (stars[i].radius <= 0) {
+		            stars.splice(i, 1);
+		        }
+		    }
+
+		    for (var i = 0; i < explosions.length; i++) {
+		        if (explosions[i].length <= 0) {
+		            explosions.splice(i, 1);
+		        }
+		    }
+
+		    timer++;
+		    // console.log(timer);
+		    if (timer % randomSpawnRate == 0) {
+		        stars.push(new Star());
+		        randomSpawnRate = Math.floor((Math.random() * 10) + 75)
+		    }
+
+		}
+
+		animate();
